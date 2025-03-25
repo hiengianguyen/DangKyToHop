ddf749e07fa8190f4ab9e76d8834b6d11a16b158
hiengianguyen <hiennguyen2060@gmail.com>
2025-03-25 22:17:13 +0700

    Config Firestore and create Firestore model

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,1 +1,12 @@
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
 
+const serviceAccount = require("./storageAccount.json");
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+module.exports = db;
