1cc7b474d73ec339564d1749b9fc1c0b7e22a99d
hiengianguyen <hiennguyen2060@gmail.com>
2025-03-28 21:22:40 +0700

    Testing deploy render

--- src/apps/controllers/WelcomeController.js
+++ src/apps/controllers/WelcomeController.js
@@ -1,3 +1,17 @@
-class WelcomeController {}
+const FirestoreModel = require("../models/FirestoreModel");
+const { UsersCollectionName } = require("../../constants");
+
+class WelcomeController {
+  constructor() {
+    this.firestoreModel = new FirestoreModel(UsersCollectionName);
+    this.index = this.index.bind(this);
+  }
+
+  // For Testing
+  async index(req, res) {
+    const users = await this.firestoreModel.getAllItems();
+    return res.json(users[0]);
+  }
+}
 
 module.exports = new WelcomeController();
