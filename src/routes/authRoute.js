f9daa92eda4a2ee0f4c1f80f809e8cfd7fd427ec
hiengianguyen <hiennguyen2060@gmail.com>
2025-04-04 18:45:41 +0700

    Set route for forms login

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,4 +2,7 @@ const AuthController = require("../apps/controllers/AuthController");
 const express = require("express");
 const router = express.Router();
 
+router.post("/signin", AuthController.signIn);
+router.post("/signup", AuthController.signUp);
+
 module.exports = router;
