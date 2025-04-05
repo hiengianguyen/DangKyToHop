d10ac91551bf180aee9b82b4dec8ce2efa2de3c3
hiengianguyen <hiennguyen2060@gmail.com>
2025-04-05 17:54:38 +0700

    Add logic for signup/signin function

--- src/apps/controllers/AuthController.js
+++ src/apps/controllers/AuthController.js
@@ -1,10 +1,56 @@
+const { FirestoreModel, UserModel } = require("../models");
+const { UsersCollectionName } = require("../../constants");
+
 class AuthController {
-  signIn(req, res, next) {
-    res.redirect("/");
+  constructor() {
+    this.userDbRef = new FirestoreModel(UsersCollectionName, UserModel);
+    this.signIn = this.signIn.bind(this);
+    this.signUp = this.signUp.bind(this);
   }
 
-  signUp(req, res, next) {
-    res.redirect("/");
+  async signIn(req, res, next) {
+    const { phone, password } = req.body;
+
+    const existedUser = await this.userDbRef.getItemByFilter({
+      phone: phone,
+      password: password
+    });
+
+    if (existedUser) {
+      console.log("Login success!!!");
+      return res.redirect("/");
+    } else {
+      console.log("Login faill!!!");
+      return res.redirect("/");
+    }
+  }
+
+  async signUp(req, res, next) {
+    const { fullName, password, secondarySchoolName, districtId, phone, dateOfBirth } = req.body;
+
+    const existedPhone = await this.userDbRef.getItemByFilter({
+      phone: phone
+    });
+
+    if (existedPhone) {
+      console.log("The phone is exited!");
+      return res.redirect("/");
+    } else {
+      const userModel = new UserModel(
+        undefined,
+        fullName,
+        password,
+        secondarySchoolName,
+        districtId,
+        phone,
+        dateOfBirth,
+        undefined,
+        undefined,
+        undefined
+      );
+      await this.userDbRef.addItem(userModel);
+      return res.redirect("/");
+    }
   }
 }
 
