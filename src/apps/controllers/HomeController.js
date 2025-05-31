9af8aec7a413a2df4f746cb4a01b47df624befd5
hiengianguyen <hiennguyen2060@gmail.com>
2025-05-31 11:10:04 +0700

    Show modal combination detail

--- src/apps/controllers/HomeController.js
+++ src/apps/controllers/HomeController.js
@@ -1,10 +1,11 @@
-const { FirestoreModel, HighSchoolModel, CombinationModel } = require("../models");
-const { HighSchoolsCollectionName, CombinationsCollectionName } = require("../../constants");
+const { FirestoreModel, HighSchoolModel, CombinationModel, SubjectModel } = require("../models");
+const { HighSchoolsCollectionName, CombinationsCollectionName, SubjectsCollectionName } = require("../../constants");
 
 class HomeController {
   constructor() {
     this.highSchoolDbRef = new FirestoreModel(HighSchoolsCollectionName, HighSchoolModel);
     this.combinationDbRef = new FirestoreModel(CombinationsCollectionName, CombinationModel);
+    this.subjectDbRef = new FirestoreModel(SubjectsCollectionName, SubjectModel);
     this.homePage = this.homePage.bind(this);
   }
 
@@ -15,14 +16,24 @@ class HomeController {
           name: "Trường THPT Duy Tân"
         });
 
+        const subjects = await this.subjectDbRef.getAllItems(false);
         const combinations = await this.combinationDbRef.getAllItems(false);
         //sort by name (asc)
         combinations.sort((a, b) => (a.name > b.name ? 1 : -1));
 
+        let compulsorySubjects = combinations[0].compulsorySubjects;
+        let optionalSubjects = combinations[0].optionalSubjects;
+
+        compulsorySubjects = subjects.filter((subject) => compulsorySubjects.includes(subject.name));
+        optionalSubjects = subjects.filter((subject) => optionalSubjects.includes(subject.name));
+
         return res.render("home", {
           role: req.query.role,
           schoolInfo: schoolInfo,
-          combinations: combinations
+          combinations: combinations,
+          combination: combinations[0],
+          compulsorySubjects: compulsorySubjects,
+          optionalSubjects: optionalSubjects
         });
       } else {
         return res.redirect(`/home?role=${req.cookies.role}`);
