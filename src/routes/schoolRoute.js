16a06fe6bb4036cf87fc018beb7b6a937f9a8f31
hiengianguyen <hiennguyen2060@gmail.com>
2025-06-04 22:08:30 +0700

    Create info contact teacher page

--- src/routes/schoolRoute.js
+++ src/routes/schoolRoute.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", SchoolController.index);
+router.get("/contact", SchoolController.contact);
 
 module.exports = router;
