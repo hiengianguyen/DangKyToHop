3228bc37b5735af181a7ebe281931fbc11b3852a
hiengianguyen <hiennguyen2060@gmail.com>
2025-06-12 18:03:52 +0700

    Small changes

--- src/utils/convertToVietnameseDateTime.js
+++ src/utils/convertToVietnameseDateTime.js
@@ -1,6 +1,5 @@
 function convertToVietnameseDateTime(dateTime) {
   const updatedAt = new Date(dateTime);
-
   const vietnameseDateTime = updatedAt.toLocaleString("vi-VN", {
     day: "2-digit", // Day of the month, 2 digits
     month: "2-digit", // Month, 2 digits
@@ -9,10 +8,8 @@ function convertToVietnameseDateTime(dateTime) {
     minute: "2-digit", // Minute in 2-digit format
     hour12: true // 12-hour format (AM/PM)
   });
-
   const [time, period, date] = vietnameseDateTime.split(" ");
   const formattedDateTime = `${time} ${period} ${date}`;
-
   return formattedDateTime;
 }
 
