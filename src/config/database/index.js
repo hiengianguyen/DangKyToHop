5e9c29fed9c9490b46a00b9e5b4c7663aef0d2ef
hiengianguyen <hiennguyen2060@gmail.com>
2025-06-12 12:04:55 +0700

    Delete comment and small refactor

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -1,6 +1,5 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
-// const serviceAccount = require("./storageAccount.json");
 require("dotenv").config();
 
 const firebaseConfig = {
