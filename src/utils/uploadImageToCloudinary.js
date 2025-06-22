4f78a5c0997c450292e7529ed64b0104ad274c3a
hiengianguyen <hiennguyen2060@gmail.com>
2025-06-23 00:39:59 +0700

    Refactor toastMessage and fetch function

--- src/utils/uploadImageToCloudinary.js
+++ src/utils/uploadImageToCloudinary.js
@@ -1,36 +1,33 @@
+import { fetchAsync } from "./fetchAsync.js";
+
 const CloudinaryConstant = {
   CloudName: "dwoymvppw",
   UploadPreset: "my_preset",
   UploadUrl: "https://api.cloudinary.com/v1_1/{cloudName}/upload"
 };
 
-function uploadImageToCloudinary(file, foder) {
-  return new Promise((resolve, reject) => {
-    if (file) {
-      const uploadUrl = CloudinaryConstant.UploadUrl.replace("{cloudName}", CloudinaryConstant.CloudName);
+export async function uploadImageToCloudinary(file, foder) {
+  if (file) {
+    const uploadUrl = CloudinaryConstant.UploadUrl.replace("{cloudName}", CloudinaryConstant.CloudName);
 
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("upload_preset", CloudinaryConstant.UploadPreset);
-      formData.append("folder", foder);
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", CloudinaryConstant.UploadPreset);
+    formData.append("folder", foder);
 
-      fetch(uploadUrl, {
-        method: "POST",
-        body: formData
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.secure_url) {
-            resolve(data.secure_url);
-          } else {
-            reject(data.error.message || "Không thể tải lên ảnh");
-          }
-        })
-        .catch((error) => {
-          reject("Lỗi tải ảnh lên Cloudinary: " + error);
-        });
+    const data = await fetchAsync(uploadUrl, "POST", formData, {});
+    if (data.secure_url) {
+      return {
+        data: data.secure_url
+      };
     } else {
-      reject("Vui lòng chọn ảnh");
+      return {
+        error: data.error.message || "Không thể tải lên ảnh"
+      };
     }
-  });
+  } else {
+    return {
+      error: "Vui lòng chọn ảnh"
+    };
+  }
 }
