a183695a71cca201f48e7fc2e80ac8393d4d540a
hiengianguyen <hiennguyen2060@gmail.com>
2025-06-23 21:31:23 +0700

    Change logic fetchAsync function common and refactor UI

--- src/utils/convertVietnameseDatetimeToDate.js
+++ src/utils/convertVietnameseDatetimeToDate.js
@@ -1,9 +1,7 @@
-function convertVieDayTimeToDate(dateTimeStr) {
+function convertVietnameseDatetimeToDate(dateTimeStr) {
   const [timePart, datePart] = dateTimeStr.split(" ");
   const [day, month, year] = datePart.split("/").map(Number);
   const [hours, minutes, seconds] = timePart.split(":").map(Number);
 
-  const date = new Date(year, month - 1, day, hours, minutes, seconds);
-
-  return date;
+  return new Date(year, month - 1, day, hours, minutes, seconds);
 }
