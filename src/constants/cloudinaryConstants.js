03af9a256d48851a7799c0a9ef077e6fc4802007
hiengianguyen <hiennguyen2060@gmail.com>
2025-06-25 23:05:43 +0700

    Delete src/constants/cloudinaryConstant.js

--- src/constants/cloudinaryConstants.js
+++
@@ -1,7 +0,0 @@
-const CloudinaryConstant = {
-  CloudName: "dwoymvppw",
-  UploadPreset: "my_preset",
-  UploadUrl: "https://api.cloudinary.com/v1_1/{cloudName}/upload"
-};
-
-module.exports = CloudinaryConstant;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,3 @@
-const CloudinaryConstant = require("./cloudinaryConstants");
 const CollectionNameConstant = require("./collectionNameConstants");
 
-module.exports = {
-  CloudinaryConstant,
-  CollectionNameConstant
-};
+module.exports = { CollectionNameConstant };
