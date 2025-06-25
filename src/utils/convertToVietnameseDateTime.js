68983f4d55bb374886c0768013c78873fa445721
hiengianguyen <hiennguyen2060@gmail.com>
2025-06-25 12:36:44 +0700

    Change format dateTime and refactor function convert time

--- src/utils/convertToVietnameseDateTime.js
+++ src/utils/convertToVietnameseDateTime.js
@@ -1,15 +1,14 @@
-function convertToVietnameseDateTime(dateTimeString) {
-  const date = new Date(dateTimeString);
-  const vietnameseDateTime = date.toLocaleString("vi-VN", {
-    day: "numeric",
-    month: "numeric",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false
-  });
-  return vietnameseDateTime;
+function convertToVietnameseDateTime(dateTime) {
+  const date = new Date(dateTime);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minute = String(date.getMinutes()).padStart(2, "0");
+  const second = String(date.getSeconds()).padStart(2, "0");
+
+  return `${hour}:${minute}:${second} ${day}/${month}/${year}`;
 }
 
 module.exports = { convertToVietnameseDateTime };
