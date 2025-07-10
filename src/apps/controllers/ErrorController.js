aec6b673d3bde2a69b0d2fb8884694efac6c0af5
hiengianguyen <hiennguyen2060@gmail.com>
2025-07-10 22:57:05 +0700

    Delete constructor function in controller ErrorController

--- src/apps/controllers/ErrorController.js
+++ src/apps/controllers/ErrorController.js
@@ -1,8 +1,4 @@
 class ErrorController {
-  constructor() {
-    this.handleErrorRoute = this.handleErrorRoute.bind(this);
-  }
-
   async handleErrorRoute(req, res, next) {
     if (req?.cookies?.isLogin === "true") {
       return res.status(404).render("404");
