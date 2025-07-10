63137df74b6a729112a3a9480c706d0145657331
hiengianguyen <hiennguyen2060@gmail.com>
2025-07-10 23:51:16 +0700

    File structure design

--- src/apps/controllers/ErrorController.js
+++ src/apps/controllers/ErrorController.js
@@ -1,9 +1,9 @@
 class ErrorController {
   async handleErrorRoute(req, res, next) {
     if (req?.cookies?.isLogin === "true") {
-      return res.status(404).render("404");
+      return res.status(404).render("notfound/notfound-user");
     } else {
-      return res.status(404).render("404-home");
+      return res.status(404).render("notfound/notfound-guest");
     }
   }
 }
