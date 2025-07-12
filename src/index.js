ee25fb8ffc89f361608680ba0501581c8500bd3d
Nguyen Gia Hien <166305613+hiengianguyen@users.noreply.github.com>
2025-07-12 08:42:51 +0700

    Update listen port log

--- src/index.js
+++ src/index.js
@@ -42,4 +42,4 @@ app.set("views", path.join(__dirname, "resources/views"));
 
 route(app);
 
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`App is listening at port ${port}`));
