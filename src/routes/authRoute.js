0ac54092c805a473564e0b4196e3d0f2c5d4b69e
hiengianguyen <hiennguyen2060@gmail.com>
2025-07-13 21:03:11 +0700

    Forgot password page

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post("/signin", AuthController.signIn);
 router.post("/signup", AuthController.signUp);
 router.get("/signout", AuthController.signOut);
+router.get("/forgot-password", AuthController.forgotPassword);
 
 module.exports = router;
