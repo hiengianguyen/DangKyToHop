cf8c7b5534f547858629e84d01f29d307e651532
Nguyen Gia Hien <166305613+hiengianguyen@users.noreply.github.com>
2025-07-19 15:03:06 +0000

    temp commit

--- src/apps/controllers/index.js
+++ src/apps/controllers/index.js
@@ -2,10 +2,15 @@ const AuthController = require("../controllers/AuthController");
 const MeController = require("../controllers/MeController");
 const HomeController = require("../controllers/HomeController");
 const CombinationController = require("../controllers/CombinationController");
+const FileController = require("../controllers/FileController");
+const CombinationController = require("../controllers/CombinationController");
+const CombinationController = require("../controllers/CombinationController");
+const CombinationController = require("../controllers/CombinationController");
 
 module.exports = {
   AuthController,
   MeController,
   HomeController,
-  CombinationController
+  CombinationController,
+  FileController
 };
