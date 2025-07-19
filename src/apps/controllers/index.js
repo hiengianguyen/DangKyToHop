8a3a291b4f88ba7634669a5df108f69e749b83df
Nguyen Gia Hien <166305613+hiengianguyen@users.noreply.github.com>
2025-07-19 15:07:08 +0000

    complete export controller

--- src/apps/controllers/index.js
+++ src/apps/controllers/index.js
@@ -3,14 +3,17 @@ const MeController = require("../controllers/MeController");
 const HomeController = require("../controllers/HomeController");
 const CombinationController = require("../controllers/CombinationController");
 const FileController = require("../controllers/FileController");
-const CombinationController = require("../controllers/CombinationController");
-const CombinationController = require("../controllers/CombinationController");
-const CombinationController = require("../controllers/CombinationController");
+const SchoolController = require("../controllers/SchoolController");
+const NotfoundController = require("../controllers/NotfoundController");
+const MaintenanceController = require("../controllers/MaintenanceController");
 
 module.exports = {
   AuthController,
   MeController,
   HomeController,
   CombinationController,
-  FileController
+  FileController,
+  SchoolController,
+  NotfoundController,
+  MaintenanceController
 };
