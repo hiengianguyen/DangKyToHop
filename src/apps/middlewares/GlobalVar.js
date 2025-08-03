678a57dcbf95faf9c8890dca6578380e6ad43ce7
hiengianguyen <hiennguyen2060@gmail.com>
2025-08-03 18:46:15 +0700

    Check user var

--- src/apps/middlewares/GlobalVar.js
+++ src/apps/middlewares/GlobalVar.js
@@ -7,9 +7,6 @@ module.exports = async (req, res, next) => {
 
   let user, submited;
   const userId = req?.cookies?.userId;
-  if (userId === "") {
-    return next();
-  }
 
   Promise.all([
     (user = await userDbRef.getItemById(userId)),
@@ -18,11 +15,13 @@ module.exports = async (req, res, next) => {
     }))
   ]);
 
+  if (user) {
+    res.locals.user = user;
+    res.locals.fullName = user.fullName;
+    res.locals.role = user.role;
+  }
   res.locals.isLogin = req?.cookies?.isLogin === "true";
   res.locals.avatar = req?.cookies?.avatar;
-  res.locals.user = user;
-  res.locals.fullName = user.fullName;
-  res.locals.role = user.role;
   res.locals.userId = userId;
   res.locals.submited = submited;
   next();
