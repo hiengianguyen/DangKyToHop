fc186ab4cd0887eb47e56a7302c98c0bd014651b
hiengianguyen <hiennguyen2060@gmail.com>
2025-08-06 21:47:40 +0700

    Feature edit/delete notification

--- src/routes/notiRoute.js
+++ src/routes/notiRoute.js
@@ -4,9 +4,12 @@ const router = express.Router();
 
 router.get("/", NotificationController.index);
 router.get("/generator", NotificationController.notiGenerator);
+router.get("/edit/:id", NotificationController.notiEdit);
 router.get("/info", NotificationController.info);
 router.post("/create-noti", NotificationController.createNoti);
+router.post("/update-noti", NotificationController.updateNoti);
 router.get("/detail/:id", NotificationController.notiDetail);
-router.get("/delete/:id", NotificationController.notiDelete);
+router.get("/delete/:id", NotificationController.userNotiDelete);
+router.get("/delete/m/:id", NotificationController.notiDelete);
 
 module.exports = router;
